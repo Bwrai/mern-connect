c9e518c30628c32a291ec87732d1f0a39fd01126
Manveer <manveer.bwrai@gmail.com>
2023-12-04 14:22:16 +0530

    Installing React with vite, tailwindcss and configuring them

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-4xl p-4 text-center text-green-700 font-bold'>Hello World! Tailwind css configured</h1>
+    </div>
+  )
+}
